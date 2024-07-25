ed204956674a8e5f287e2d772731874d90fed60d
shishira <your_email@example.com>
2024-07-25 17:33:03 +0530

    puddle-log

--- server.py
+++ server.py
@@ -13,6 +13,13 @@ from image_to_text import puddle_ocr
 import sys
 from groq_api import  groq_api_response
 import json
+import logging
+
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, 
+                    format='%(asctime)s %(levelname)s %(message)s', 
+                    handlers=[logging.FileHandler('paddleocr_debug.log'), logging.StreamHandler()])
 
 app = FastAPI()
 
@@ -21,11 +28,16 @@ def test():
     return json.loads(groq_api_response("sathish"))
 
 @app.post('/test_ocr')
-def test_ocr(file:UploadFile):
-    with open('./temp/temp.jpeg','wb') as f:
-       f.write(file.file.read())
-    data = puddle_ocr('./temp/temp.jpeg') 
-    return data
+async def test_ocr(file:UploadFile):
+    try:
+        with open('./temp/temp.jpeg','wb') as f:
+            f.write(file.file.read())
+        data =  puddle_ocr('./temp/temp.jpeg') 
+        logging.debug(f'OCR result: {data}')
+        return data
+    except Exception as e:
+        print(f'error during process:{e}')
+        
     
 @app.post('/upload_file')
 async def image_upload(file:UploadFile):
