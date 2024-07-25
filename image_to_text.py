0081ca8cc3fb18d9034aa2e0c07f0ac662404b07
shishira <your_email@example.com>
2024-07-25 16:47:14 +0530

    paddleocr parameters

--- image_to_text.py
+++ image_to_text.py
@@ -7,12 +7,25 @@ Created on Wed Jul 24 14:18:47 2024
 """
 
 from paddleocr import PaddleOCR
-
-
+ocr_model = PaddleOCR(
+        lang='en',
+                  drop_score=0,
+                  use_angle_cls=False,
+                  cls=False,
+                  rec_model_dir='paddle_files/default/rec/en/en_PP-OCRv3_rec_infer',
+                  det_model_dir='paddle_files/default/det/en/en_PP-OCRv3_det_infer',
+                  cls_model_dir='paddle_files/default/cls/ch_ppocr_mobile_v2.0_cls_infer',
+                  det_db_box_thresh=0.3,
+                  enable_mkldnn=True,
+                  use_gpu=False
+        
+        
+        
+    )
 
 
 def puddle_ocr(image):
-    ocr_model = PaddleOCR(lang= 'en',use_angle_cls=True)
+
     full_string = ''
     text = ocr_model.ocr(image)
     t_0 = text[0]
